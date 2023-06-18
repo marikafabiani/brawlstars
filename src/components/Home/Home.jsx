ff51d90df4a977748358712f34d02e1fb5c6e182
marikafabiani <marikafabiani63@gmail.com>
2023-06-18 11:49:09 +0200

    pagina login

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+export default function HomePage() {
+  return (
+    <>
+      <h1>Hey from HomePage</h1>
+      <p>Eccoci sul sitello di Brawl Stars dei mu!</p>
+    </>
+  );
+}
