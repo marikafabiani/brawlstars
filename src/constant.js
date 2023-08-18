deb8fd3198b3f1588e07e8cd46cf1cc0d863ea75
Diegomaths <diegobellazzi@gmail.com>
2023-08-18 22:07:38 +0200

    set host online in constant.js

--- src/constant.js
+++ src/constant.js
@@ -1,3 +1,3 @@
-const host = "http://localhost:5000/api/v1/";
-//const  host = "https://brawlstars-be.ew.r.appspot.com/api/v1/";
+// const host = "http://localhost:5000/api/v1/";
+const  host = "https://brawlstars-be.ew.r.appspot.com/api/v1/";
 export default host
